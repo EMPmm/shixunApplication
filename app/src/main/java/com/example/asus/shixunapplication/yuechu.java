a5fbb6b2084201df1a5cd18ba9d48f6b667d8087
EMPmm <1322443197@qq.com>
2018-07-10 12:01:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus/shixunapplication/yuechu.java
@@ -0,0 +1,13 @@
+package com.example.asus.shixunapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class yuechu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yuechu);
+    }
+}
